e99178336413f639d51c16ce30686033cd1f50e4
tarunsaran <tarunsaran080@gmail.com>
2024-05-07 14:05:05 +0530

    adding new file

--- sample.py
+++ sample.py
@@ -1,1 +1,10 @@
-print('hello')
+s = input("enter sting ")
+upper_case =0
+lower_case = 0
+for char1 in s:
+    if char1.isupper():
+        upper_case+=1
+    elif char1.islower():
+        lower_case+=1 
+print("lowe_case:",lower_case)
+print("upper_case:",upper_case)
